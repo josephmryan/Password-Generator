46ed2af9535c458b713ee2190e4eb3760a93e965
Joe R <joeryansemail@gmail.com>
2022-03-31 20:36:35 -0400

    resubmit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,40 @@
 // Assignment code here
 function generatePassword() {
-  // I created each character set separately as an array and depending on the user's answer choices, I will append the set to a blank array that I created.
+  // Each character set in an array that will be put into a new array based on selections
+  var numSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var lowerCaseSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var upperCaseSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var numSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var specialSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
   var selectedArray = [];
 
   var passwordLength = getPasswordLength();
 
   var charTypeSelected = false;
-  // This loop ensures the user selects at least one character type
-  while (charTypeSelected == false) {
+  // This makes the user select at least one character type
+  while (charTypeSelected == false) {    
+    var numericCharacters = getChoice("numeric");
     var lowerCase = getChoice("lowercase");
     var upperCase = getChoice("uppercase");
-    var numericCharacters = getChoice("numeric");
     var specialCharacters = getChoice("special");
-    if ((lowerCase) || (upperCase) || (numericCharacters) || (specialCharacters)) {
+    if ((numericCharacters) || (lowerCase) || (upperCase)|| (specialCharacters)) {
       charTypeSelected = true;
     } else {
       window.alert("You must select at least one character type.")
     }
   }
-
-  // These if statements determine the user choices and then append them to the blank array I created.
+  // These if statements determine the user choices and then append them to the blank array 
+  if (numericCharacters) {
+    selectedArray = selectedArray.concat(numSet);
+  }
   if (lowerCase) {
     selectedArray = selectedArray.concat(lowerCaseSet);
   }
   if (upperCase) {
     selectedArray = selectedArray.concat(upperCaseSet);
   }
-  if (numericCharacters) {
-    selectedArray = selectedArray.concat(numSet);
-  }
   if (specialCharacters) {
     selectedArray = selectedArray.concat(specialSet);
   }
-
   var passwordString = "";
   // This loop will take the appended array, randomly select elements from it, then append the selections to a string, generating the password.
   for (var i = 0; i < passwordLength; i++) {
@@ -50,48 +48,39 @@ function getPasswordLength() {
   var userChoice = 0;
   while ((userChoice < 8) || (userChoice > 128)) {
     userChoice = parseInt(window.prompt("Enter the number of characters between 8 and 128: "));
-
     // Checking here to make sure the user entered a number and not a letter.
     if (isNaN(userChoice)) {
       // This will reset the choice value to 0 so it can restart the loop if the user entered anything besides a number.
       userChoice = 0;
     }
   }
-
   return userChoice;
 }
-
 // Created this function as the user choice options are repetitive and this simplifies the code needed.
 function getChoice(currentOption) {
-  var userChoice = "a",
+  var userChoice = "x",
     messagePrompt = "";
   var messagePrompt = ('Would you like '.concat(currentOption));
-  messagePrompt = messagePrompt.concat(' characters (y/n)?');
+  messagePrompt = messagePrompt.concat(' characters? Enter yes or no');
   // This loop ensures the user enters a valid response.
-  while (userChoice = "a") {
+  while (userChoice = "x") {
     userChoice = (window.prompt(messagePrompt));
     // Added the line below for ease of usability on mobile devices as some of them automatically capitalize when entering input.
     userChoice = userChoice.toLowerCase();
-    if (userChoice == "y") {
+    if (userChoice == "yes") {
       return true;
-    } else if (userChoice == "n") {
+    } else if (userChoice == "no") {
       return false;
     }
   }
 }
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
